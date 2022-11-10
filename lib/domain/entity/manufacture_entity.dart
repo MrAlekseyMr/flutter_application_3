3c93c5c13da57f7c0501d091a453eada6983e979
Алексей Сергеевич <isip_a.s.opeykin@mpt.ru>
2022-11-10 19:57:34 +0300

    1

--- lib/domain/entity/manufacture_entity.dart
+++ lib/domain/entity/manufacture_entity.dart
@@ -7,17 +7,3 @@ class ManufactureEntity {
     required this.namemanufacture,
   });
 }
-
-enum ManufactureEnum {
-  nestle(id: 1, namemanufacture: 'Nestle'),
-  mars(id: 2, namemanufacture: 'Mars'),
-  miratorg(id: 3, namemanufacture: 'Мираторг');
-
-  const ManufactureEnum({
-    required this.id,
-    required this.namemanufacture,
-  });
-
-  final int id;
-  final String namemanufacture;
-}
